42142eabc22614f937ad749ee098e44261cb9dde
Mihir <email.com>
2023-04-30 09:42:13 +0530

    refactor some code

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -5,10 +5,10 @@ const TodoList = ({todos,handleEdit,handleDelete}) => {
     <ul className='allTodos'>
           { 
             todos.map((t) => (
-              <li className='singleTodo'>
-                <span className='todoText' key={t.id}>{t.todo}</span>
-                <button onClick={() => handleEdit(t.id)}>Edit</button>
-                <button onClick={() => handleDelete(t.id)}>Delete</button>
+              <li className = 'singleTodo'>
+                <span className = 'todoText' key={t.id}>{t.todo}</span>
+                <button onClick = {() => handleEdit(t.id)}>Edit</button>
+                <button onClick = {() => handleDelete(t.id)}>Delete</button>
               </li>
             ))
           }
